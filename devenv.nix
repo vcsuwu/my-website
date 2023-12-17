abafb8129131758aa31e29f54005978725de42e9
vcsuwu <zenyabtw@gmail.com>
2023-12-18 00:19:12 +0500

    initial commit

---
+++ devenv.nix
@@ -0,0 +1,29 @@
+{ pkgs, config, ... }:
+
+{
+  languages.php = {
+    enable = true;
+    version = "8.1";
+    ini = ''
+      memory_limit = 256M
+    '';
+    fpm.pools.web = {
+      settings = {
+        "pm" = "dynamic";
+        "pm.max_children" = 5;
+        "pm.start_servers" = 2;
+        "pm.min_spare_servers" = 1;
+        "pm.max_spare_servers" = 5;
+      };
+    };
+  };
+
+  services.caddy.enable = true;
+  services.caddy.virtualHosts."http://localhost:8000" = {
+    extraConfig = ''
+      root * ${config.env.DEVENV_ROOT}/basic/web
+      php_fastcgi unix/${config.languages.php.fpm.pools.web.socket}
+      file_server
+    '';
+  };
+}
